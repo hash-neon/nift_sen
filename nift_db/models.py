f08f08675f318f85e9742658331cf5cb3b6e5f15
HARSH <harsh@Harsh-Lenovo.(none)>
2012-03-21 01:47:10 +0530

    version_1.1

--- nift_db/models.py
+++ nift_db/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	SEX_CHOICES = (
+		('M','Male'),
+		('F','Female'),
+	)
+
+	STATUS_CHOICES = (
+		('Married',''),
+		('Unmarried',''),
+	)
+
+	first_name 	= models.CharField(max_length=100)
+	last_name  	= models.CharField(max_length=100)
+	login_name 	= models.CharField(max_length=100, unique=True) 
+      	sex        	= models.CharField(max_length=1 ,choices=SEX_CHOICES)
+	password   	= models.CharField(max_length=100)
+	email_add  	= models.EmailField(max_length=100)
+	dob 	   	= models.DateField()
+	phone_no   	= models.SmallIntegerField()
+	user_id    	= models.CharField(max_length=100, primary_key=True)	
+	marital_status  = models.CharField(max_length=100, choices=STATUS_CHOICES)
+
+class Attendance(models.Model):
+	date 		= models.DateField(auto_now=True)
+	present 	= models.BooleanField()
+	user_id 	= models.ForeignKey(User)
+	user_id.primary_key = True
--- settings.py
+++ settings.py
@@ -11,9 +11,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'SEN',                      # Or path to database file if using sqlite3.
+        'USER': 'harsh',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nift_db'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
